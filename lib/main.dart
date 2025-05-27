dad08da62cb819fa04d5912310963128e89f629b
Eric N. Garcia <eng618@garciaericn.com>
2025-05-27 02:23:28 -0400

    feat: update application title to 'Home Harmony'

--- lib/main.dart
+++ lib/main.dart
@@ -261,7 +261,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Home Harmony',
       theme: ThemeData(
         // This is the theme of your application.
         //

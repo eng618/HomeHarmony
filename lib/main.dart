2ba194cb5506981441a18bb05807d26fb3a8a941
Eric N. Garcia <eng618@garciaericn.com>
2025-05-27 01:20:00 -0400

    feat: add dotenv configuration for Firebase and create example .env file

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,11 @@ import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
